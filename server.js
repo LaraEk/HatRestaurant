6d610617f3fb3ef4038d3358b208497f79be5ab6
Ekscursionist <larakek@gmail.com>
2018-08-04 12:54:07 -0400

    making the backend work - routes, reservations, etc

---
+++ server.js
@@ -0,0 +1,85 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8082;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Star Wars Characters (DATA)
+// =============================================================
+var tables = [
+      {
+    routeName: "madmax",
+    name: "Mad Max",
+    phone_number: "555-555-5555",
+    email: "mad@max.com",
+    unique_id: "5"
+  }
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reservation", function(req, res) {
+  res.sendFile(path.join(__dirname, "reservation.html"));
+});
+
+app.get("/view", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+// Displays all reservations
+app.get("/api/tables", function(req, res) {
+  return res.json(tables);
+});
+
+// // Displays a single character, or returns false
+// app.get("/api/characters/:character", function(req, res) {
+//   var chosen = req.params.character;
+
+//   console.log(chosen);
+
+//   for (var i = 0; i < characters.length; i++) {
+//     if (chosen === characters[i].routeName) {
+//       return res.json(characters[i]);
+//     }
+//   }
+
+//   return res.json(false);
+// });
+
+// Create New Characters - takes in JSON input
+app.post("/api/tables", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newreservation = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newreservation.routeName = newreservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newreservation);
+
+  tables.push(newreservation);
+
+  res.json(newreservation);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
